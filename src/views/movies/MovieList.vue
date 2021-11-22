742557f2abdeec7e21ef21e7063af24f77186ac2
okdongdong <kjch6411@naver.com>
2021-11-22 09:17:02 +0900

    [EDIT] 페이지네이션 인터페이스 수정

--- src/views/movies/MovieList.vue
+++ src/views/movies/MovieList.vue
@@ -1,26 +1,42 @@
 <template>
-  <div>
-    <div >
-      <SearchInput />
-      </div>
-    <MovieCardMatrix :movieList="movies" />
-    <!-- <v-list v-for="(movieList, idx) in movies" :key='idx'>
-    </v-list> -->
-     <v-item-group v-model="onboarding" class="text-center" mandatory>
-      <v-item v-for="n in length" :key="`btn-${n}`">
-        <v-icon v-if="onboarding === n - 1" color="teal darken-2"
-          >mdi-record</v-icon
+  <div class="my-5">
+    <div class="text-center" mandatory>
+      <span v-for="n in 5" :key="`btn-${n}`">
+        <span @click="page = n">
+          <v-icon v-if="page === n" color="teal darken-2">mdi-record</v-icon>
+          <v-icon v-else color="white">mdi-record</v-icon>
+        </span>
+      </span>
+    </div>
+    <div class="d-flex">
+      <button id="left-btn">
+        <v-icon size="100" color="white" @click="prev">
+          mdi-chevron-left
+        </v-icon>
+      </button>
+      <MovieCardMatrix :movieList="movies" />
+      <button id="right-btn">
+        <v-icon
+          :disabled="page === 1000"
+          size="100"
+          color="white"
+          depressed
+          @click="next"
         >
-        <v-icon v-else color="white">mdi-record</v-icon>
-      </v-item>
-    </v-item-group>
+          mdi-chevron-right
+        </v-icon>
+      </button>
+      <v-dialog
+        transition="dialog-top-transition"
+        max-width="600"
+      >으앙</v-dialog>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
 import MovieCardMatrix from "../../components/movies/MovieCardMatrix.vue";
-import SearchInput from "../../components/SearchInput.vue";
 
 export default {
   name: "MovieList",
@@ -35,7 +51,10 @@ export default {
   },
   components: {
     MovieCardMatrix,
-    SearchInput,
+  },
+  updated: function () {
+    document.querySelector("#left-btn").classList.add("left-btn");
+    document.querySelector("#right-btn").classList.add("right-btn");
   },
   created: function () {
     this.getMovie();
@@ -65,13 +84,30 @@ export default {
           console.log(err);
         });
     },
+    next: function () {
+      this.page = this.page + 1 === 1000 ? 1000 - 1 : this.page + 1;
+    },
+    prev: function () {
+      this.page = this.page - 1 < 1 ? 1 : this.page - 1;
+    },
   },
-  computed: {
-    pageCount: function () {
-      return Math.ceil(this.movies.length / this.dataPerPage);
+  watch: {
+    page: function () {
+      this.getMovie();
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+.left-btn {
+  transition-delay: 500ms;
+  transition-duration: 500ms;
+  transform: translateX(9vw);
+}
+.right-btn {
+  transition-delay: 500ms;
+  transition-duration: 500ms;
+  transform: translateX(-9vw);
+}
+</style>
